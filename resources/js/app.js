e8ef186e26701b9b832d598ba125d00437e663b0
Muhammad A. Mwinchande <ammwinchande@gmail.com>
2020-03-20 08:14:31 +0300

    added font icons, installed vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
 require('./bootstrap');
+
+window.Vue = require('vue');
+
+Vue.component('current-weather', require('./components/CurrentWeather.vue').default);
+
+
+const app = new Vue({
+  el: '#app',
+});
